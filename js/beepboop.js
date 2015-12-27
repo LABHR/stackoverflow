479c0a914087be72cb844c39ed78fce5222b62af
Katie McLaughlin <katie@glasnt.com>
2015-12-27 22:05:41 +1100

    Use dhg/Skeleton for digital style

---
+++ js/beepboop.js
@@ -0,0 +1,60 @@
+function getParameterByName(name) {
+  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"), results = regex.exec(location.search);
+  return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+function search() { 
+  tag = getParameterByName("tag")
+
+  if (tag) { 
+  
+    $("#display_tag").text("Results for "+tag)
+
+    console.log(tag)
+
+    run_SO_call("top-askers", tag)
+    run_SO_call("top-answerers", tag)
+  } 
+}
+
+function display_SO_data(verb, data) { 
+  console.log(data)
+  $.each(data.items, function(d, i) { 
+    flair = '<a href="http://stackoverflow.com/users/'+i.user.user_id+'"><img src="http://stackoverflow.com/users/flair/'+i.user.user_id+'.png?theme=clean" width="208" height="58" alt="" /></a>'
+    $("#"+verb).append(flair)
+  })
+} 
+
+function run_SO_call(verb, tag) { 
+  url = "https://api.stackexchange.com/2.2/tags/"+tag+"/"+verb+"/all_time?page=1&pagesize=100&site=stackoverflow"
+  $("#"+verb+"_loading").show()
+
+  $.ajax({
+    url: url,
+    success: function(data, s, x){
+      display_SO_data(verb, data)
+    }
+  });
+}
+
+$(document).ready(function() { 
+  $("#search").submit(function(event) { 
+    event.preventDefault();
+    tag = $("#tag").val()
+    window.location.search = "tag="+tag
+  })
+  
+  search() 
+
+})
+
+$(document).ajaxStart(function() { 
+  $("#results").hide()
+  $("#loading").show()
+})
+
+$(document).ajaxStop(function() { 
+  $("#results").show()
+  $("#loading").hide()
+})
